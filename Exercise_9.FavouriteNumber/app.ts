8ba8ca193a04f953dde0d3a9372d47129247e1b6
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-03-24 23:24:05 +0500

    typescript_45assignments

--- Exercise_9.FavouriteNumber/app.ts
+++ Exercise_9.FavouriteNumber/app.ts
@@ -1,5 +1,5 @@
 /* Task_9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.*/
-let favNumber= Number(2);
+let favNumber= 2;
 let message: String = "It is the first even number,that is the reason my favourite number is"
 console.log(`${message} : ${favNumber}`);
 
