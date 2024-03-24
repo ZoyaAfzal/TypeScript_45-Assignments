88cddab26c5a4207d8a06f83f52251abaa2c4cea
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-03-24 23:17:40 +0500

    typescript_45assignments

--- Exercise_10.AddingComments/app.js
+++ Exercise_10.AddingComments/app.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /* Taskno.10 .Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs
 are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.*/
 let myName = "Zoya";
-let currentDate = new Date("2024-02-18");
+let currentDate = new Date();
 //if someone asks what is your name? my answer should be:
 console.log(`My name is ${myName}`);
 /*if someone asks what is the date today and what are your plans for today,
--- Exercise_10.AddingComments/app.ts
+++ Exercise_10.AddingComments/app.ts
@@ -3,7 +3,7 @@ import { TransformStreamDefaultController } from "stream/web";
 /* Taskno.10 .Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs 
 are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.*/
 let myName : string = "Zoya";
-let currentDate = new Date("2024-02-18");
+let currentDate = new Date();
 
 //if someone asks what is your name? my answer should be:
 console.log(`My name is ${myName}`);
