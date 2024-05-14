82ff67a0907cc233173fc509916f9f2c4756d366
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-05-14 18:43:13 +0500

    typescript45assignments

--- Exercise_4.FamousQuote/app.ts
+++ Exercise_4.FamousQuote/app.ts
@@ -1,2 +1,9 @@
-//Find a quote from a famous person you admire. Print the quote and the name of it's author including the quotation marks:
-console.log( 'Abdul Sattar Edhi once said, "My religion is humanitarianism, which is the basis of every religion in the World"');
+/*Q4: Famous Quote : Find a quote from a famous person you admire. Print the quote and the name of it's 
+author. Your output should look like the following including the quotation marks:
+Albert Einstein once said, "A person who never made a mistake never tried anything new".*/
+
+
+let quote : string = "My religion is humanitarianism, which is the basis of every religion in the World.";
+let author : string = "Abdul Sattar Edhi"; 
+
+console.log(`${author} once said, "${quote}"`);
