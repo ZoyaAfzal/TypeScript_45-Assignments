ba515dffa8b4c7aaebf2d1946fa683eae31a7d0b
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-05-14 18:24:20 +0500

    typescript45assignments

--- Exercise_3.NameCases/app.js
+++ Exercise_3.NameCases/app.js
@@ -1,6 +1,9 @@
 "use strict";
 //Store a person's name in a variable, and then print that person's name in lowercase,uppercase and titlecase.
-let personName = "Eric";
+let personName = "Zoya Afzal";
+//loweCase
 console.log(personName.toLowerCase());
+//UpperCase
 console.log(personName.toUpperCase());
-console.log(personName.split(""));
+//TitleCase
+console.log(personName.replace(/\b\w/g, c => c.toUpperCase()));
--- Exercise_3.NameCases/app.ts
+++ Exercise_3.NameCases/app.ts
@@ -1,5 +1,11 @@
 //Store a person's name in a variable, and then print that person's name in lowercase,uppercase and titlecase.
-let personName: String ="Eric";
+let personName: String = "Zoya Afzal";
+
+//loweCase
 console.log(personName.toLowerCase());
+
+//UpperCase
 console.log(personName.toUpperCase());
-console.log(personName.split(""));
+
+//TitleCase
+console.log(personName.replace(/\b\w/g,c=>c.toUpperCase()));
