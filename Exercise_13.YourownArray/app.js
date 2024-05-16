ef45b999625cc66b456fbe430fc6b21151a6086c
Zoya Afzal <zoyakhan4242.zk@gmail.com>
2024-05-16 08:44:57 +0500

    typescript45assignments

--- Exercise_13.YourownArray/app.js
+++ Exercise_13.YourownArray/app.js
@@ -2,7 +2,7 @@
 /*Task no13. Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
 Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”*/
 let vehicles = ["Ferrari296GTB", "BMW X6", "Audi Rs e-tron GT", "Rolls-Royce 2023"];
-console.log(vehicles[2] + " & " + vehicles[3] + "are the most luxurious cars");
-console.log("I wish i can buy " + vehicles[1]);
-console.log("My most favourite car is " + vehicles[0]);
-vehicles.map((items) => console.log(`I would like to own  ${items}`));
+vehicles.map((items) => console.log(`${items} are the most luxurious cars.`));
+vehicles.map((items) => console.log(`I wish i can buy ${items}.`));
+vehicles.map((items) => console.log(`My most favourite car is ${items}.`));
+vehicles.map((items) => console.log(`I would like to own  ${items}.`));
--- Exercise_13.YourownArray/app.ts
+++ Exercise_13.YourownArray/app.ts
@@ -2,7 +2,7 @@
 Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”*/
 let vehicles : String[] = ["Ferrari296GTB", "BMW X6" ,"Audi Rs e-tron GT", "Rolls-Royce 2023"];
 
-console.log(vehicles[2] + " & " + vehicles[3] + "are the most luxurious cars");
-console.log("I wish i can buy " + vehicles[1]);
-console.log("My most favourite car is " + vehicles[0]);
-vehicles.map((items) => console.log(`I would like to own  ${items}`));
+vehicles.map((items) => console.log(`${items} are the most luxurious cars.`));
+vehicles.map((items) => console.log(`I wish i can buy ${items}.`));
+vehicles.map((items) => console.log(`My most favourite car is ${items}.`));
+vehicles.map((items) => console.log(`I would like to own  ${items}.`));
